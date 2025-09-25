566285d2d5120ddf71c59563df7cda7a144b48f4
Maxime ANDRÉ <maxime.andre1986@gmail.com>
2025-09-25 23:14:43 +0200

    Switch to mqtt

--- Raspberry_Pi/rinkin/rov.py
+++ Raspberry_Pi/rinkin/rov.py
@@ -1,42 +1,51 @@
 #!/usr/bin/python3
 import asyncio
 from smbus2 import SMBus
-from pythonosc import udp_client
-from pythonosc.osc_server import AsyncIOOSCUDPServer
-from pythonosc.dispatcher import Dispatcher
-import serial
+from paho.mqtt import client as mqtt_client
+from paho.mqtt.enums import CallbackAPIVersion
+#import serial
 from cmps12 import CMPS12
 
-async def imu_loop():
+async def imu_loop(client):
     imu = CMPS12(SMBus(1))
-    client = udp_client.SimpleUDPClient("192.168.4.1", 7770)
     while True:
         imu.update()
         print(f"heading = {imu.heading()}\tpich={imu.pitch()}\troll={imu.roll()}")
-        client.send_message("/imu/heading", imu.heading())
-        client.send_message("/imu/pitch", imu.pitch())
-        client.send_message("/imu/roll", imu.roll())
+        client.publish("/imu/heading", imu.heading())
+        client.publish("/imu/pitch", imu.pitch())
+        client.publish("/imu/roll", imu.roll())
+        
         await asyncio.sleep(0.1)
 
+def on_mqtt_connect(client, userdata, flags, rc, properties):
+    print(f"on_mqtt_connect, rc={rc}")
+    client.subscribe("/motor")
 
-ser = serial.Serial("/dev/ttyS0", 115200)
+def on_message(client, userdata, msg):
+    print(f"topic: {msg.topic}, payload: {msg.payload.decode()}")
+    
+
+client = mqtt_client.Client(CallbackAPIVersion(2), client_id="raspberry-pi")
+client.on_connect = on_mqtt_connect
+client.on_message = on_message
+client.connect("localhost", 1883)
+
+client.loop_start()
+
+async def main():
+    imu_task = asyncio.create_task(imu_loop(client))
+    await imu_task
+
+asyncio.run(main())
+
+"""
 
 def motor(address, *args):
     print(f"{address:}: {args}")
     ser.write(int.to_bytes(args[0]))
 
-
-dispatcher = Dispatcher()
-dispatcher.map("/motor", motor)
-
-
-async def main():
-    imu_task = asyncio.create_task(imu_loop())
-    server = AsyncIOOSCUDPServer(("0.0.0.0", 7770), dispatcher, asyncio.get_event_loop())
-    transport, protocol = await server.create_serve_endpoint()
-    await imu_task
-    transport.close()
-
-asyncio.run(main())
+ser = serial.Serial("/dev/ttyS0", 115200)
 
 ser.close()
+
+"""
